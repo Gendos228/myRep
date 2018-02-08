875db9879b326239ae09c9a3f63e323db7df3258
Ilja <admin@topmemas.top>
2018-02-08 16:30:41 +0300

    init

---
+++ TextFormatter/MainClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFormatter;
+
+namespace TextFormatter
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris elementum eu est in mattis. Sed ullamcorper sapien nec lectus elementum, in pellentesque urna ultrices. Curabitur id nisl non metus ultricies blandit. Praesent tincidunt, felis sed facilisis convallis, erat ipsum lacinia massa, sed ullamcorper felis lorem eget ligula. Proin ultrices sagittis velit, mattis ullamcorper tellus posuere vitae. Aliquam mollis eros sed fermentum consequat. In aliquam, nibh a posuere accumsan, mauris lorem ultricies metus, et ultrices nisi ex id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer in sapien eu magna auctor finibus. Aliquam molestie eget sapien sit amet tincidunt.";
+            string outString = TextFormatter.Justify(text, 30);
+            Console.WriteLine(outString);
+        }
+    }
+}
---
+++ TextFormatter/TextFormatter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFormatter
+{
+    public class TextFormatter
+    {
+        public static string Justify(string text, int width)
+        {
+            int position = 0;
+            string resultString = "";
+            text = text.Trim();
+            
+            while(true)
+            {
+                if (text.Length - position <= width)
+                    return resultString + text.Substring(position);
+
+                string substring = text.Substring(position, width);
+                if (!substring.Contains(" ") && text.Length - 1 > position + width && text[position + width] != ' ')
+                {
+                    int endLongWord = text.Substring(position).IndexOf(" ");
+                    endLongWord = endLongWord == -1 ? text.Length : endLongWord;
+                    substring = text.Substring(position, endLongWord - position);
+                    position += endLongWord + 1;
+                    resultString += substring;
+                    if (position >= text.Length)
+                        return resultString;
+                    resultString += '\n';
+                    continue;
+                }
+                if (text[position + width] == ' ')
+                {
+                    position += width + 1;
+                    resultString += substring + '\n';
+                    continue;
+                }
+
+                position += substring.LastIndexOf(' ') + 1;
+                substring = substring.Remove(substring.LastIndexOf(' '));
+
+                int spaceNeed = width - substring.Length;
+                int spaces = substring.Split(' ').Length - 1;
+                resultString += getSpacedString(substring, spaceNeed, spaces) + '\n';
+            }
+        }
+
+        
+
+        private static string getSpacedString(string substring, int spaceNeed, int spaces)
+        {
+            string[] words = substring.Split(' ');
+            string resultString = words[0];
+            int count = spaces;
+            for (int i = 0; i < count; i++)
+            {
+                int ceilingValue = (int)(Math.Ceiling((Double)spaceNeed / spaces));
+                resultString = resultString.PadRight(resultString.Length + ceilingValue + 1);
+                resultString += words[i + 1];
+                spaceNeed -= ceilingValue;
+                spaces--;
+            }
+            return resultString;
+        }
+    }
+}
